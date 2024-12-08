9d5c3e42bfa467b3acc06acaacf23b34662b7b44
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-12-08 08:07:41 +0330

    Fixed Return Type: Fixed the issue with void* return in realloc

--- memory.h
+++ memory.h
@@ -11,7 +11,7 @@ void gc_mark(void);
 void gc_free(void *memory);
 void *gc_decref(void *memory);
 void *gc_incref(void *memory);
-void gc_realloc(void *memory,size_t new_size);
+void *gc_realloc(void *memory,size_t new_size);
 void *gc_alloc(size_t size);
 GCObject *new_gc_object(GCObject **objects);
 void gc_init(void);
