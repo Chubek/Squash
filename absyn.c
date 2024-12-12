e50cdf9d0f1ee5b961381394b0209609ec32aaff
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-12-12 06:04:52 +0330

    Added Delete Functions to AST Nodes

--- absyn.c
+++ absyn.c
@@ -17,27 +17,32 @@ ASTWord *new_ast_word(const uint8_t *buffer, size_t length) {
   return word;
 }
 
+void delete_ast_word(ASTWord *word) {
+  gc_free(word->buffer);
+  gc_decref(word);
+}
+
 ASTWord *ast_digit_to_word(long digit) {
   switch (digit) {
-  case '0':
+  case 0:
     return new_ast_word("0", 1);
-  case '1':
+  case 1:
     return new_ast_word("1", 1);
-  case '2':
+  case 2:
     return new_ast_word("2", 1);
-  case '3':
+  case 3:
     return new_ast_word("3", 1);
-  case '4':
+  case 4:
     return new_ast_word("4", 1);
-  case '5':
+  case 5:
     return new_ast_word("5", 1);
-  case '6':
+  case 6:
     return new_ast_word("6", 1);
-  case '7':
+  case 7:
     return new_ast_word("7", 1);
-  case '8':
+  case 8:
     return new_ast_word("8", 1);
-  case '9':
+  case 9:
     return new_ast_word("9", 1);
   default:
     break;
@@ -52,11 +57,27 @@ ASTSimpleCommand *new_ast_simple_command(const ASTWord *argv0) {
   return simplecmd;
 }
 
+void delete_ast_simple_command(ASTSimpleCommand *simplecmd) {
+  if (simplecmd->redir != NULL)
+    delete_ast_redir(simplecmd->redir);
+  delete_ast_word_chain(simplecmd->argv);
+  gc_decref(simplecmd);
+}
+
 void ast_word_append(ASTWord *word, const ASTWord *new_word) {
   ASTWord *tmp = word;
   while (tmp->next != NULL)
     tmp = tmp->next;
   tmp->next = gc_incref((void *)new_word);
+}
+
+void delete_ast_word_chain(ASTWord *head) {
+  ASTWord *tmp = head;
+  while (tmp) {
+    ASTWord *to_free = tmp;
+    tmp = tmp->next;
+    delete_ast_word(to_free);
+  }
 }
 
 ASTCommand *new_ast_command(enum CommandKind kind, void *new_cmd) {
@@ -67,10 +88,36 @@ ASTCommand *new_ast_command(enum CommandKind kind, void *new_cmd) {
     cmd->v_simplecmd = new_cmd;
 }
 
+void delete_ast_command(ASTCommand *cmd) {
+  if (cmd->kind == COMMAND_Simple)
+    delete_ast_simple_cmd(cmd->v_simplecmd);
+  gc_decref(cmd);
+}
+
 ASTRedir *new_ast_redir(enum RedirKind kind, ASTWord *subj) {
   ASTRedir *redir = gc_alloc(sizeof(ASTRedir));
   redir->kind = kind;
   redir->subj = subj;
   redir->fno = -1;
   return redir;
+}
+
+void delete_ast_redir(ASTRedir *redir) { delete_ast_word(redir->subj); }
+
+ASTShtoken *new_ast_shtoken(enum ShtokenKind kind, void *new_shtoken) {
+  ASTShtoken *shtoken = gc_alloc(sizeof(ASTShtoken));
+  shtoken->kind = kind;
+
+  if (shtoken->kind == SHTOKEN_Word)
+    shtoken->v_word = new_shtoken;
+  else if (shtoken->kind == SHTOKEN_Redir)
+    shtoken->v_redir = new_shtoken;
+}
+
+void delete_ast_shtoken(ASTShtoken *shtoken) {
+  if (shtoken->kind == SHTOKEN_Word)
+    delete_ast_word(shtoken->v_word);
+  else if (shtoken->kind == SHTOKEN_Redir)
+    delete_ast_redir(shtoken->v_redir);
+  gc_decref(shtoken);
 }
--- absyn.h
+++ absyn.h
@@ -44,12 +44,30 @@ typedef struct ASTCommand {
   };
 } ASTCommand;
 
+typedef struct ASTShtoken {
+  enum ShtokenKind {
+    SHTOKEN_Word,
+    SHTOKEN_Redir,
+  } kind;
+
+  union {
+    ASTWord *v_word;
+    ASTRedir *v_redir;
+  };
+} ASTShtoken;
+
 ASTWord *new_ast_word(const uint8_t *buffer, size_t length);
+void delete_ast_word(ASTWord *word);
 ASTWord *ast_digit_to_word(long digit);
 void ast_word_append(ASTWord *word, const ASTWord *new_word);
+void delete_ast_word_chain(ASTWord *head);
 ASTSimpleCommand *new_ast_simple_command(const ASTWord *argv0);
+void delete_ast_simple_command(ASTSimpleCommand *simplecmd);
 ASTCommand *new_ast_command(enum CommandKind kind, void *new_cmd);
+void delete_ast_command(ASTCommand *cmd);
 ASTRedir *new_ast_redir(enum RedirKind kind, ASTWord *subj);
-
+void delete_ast_redir(ASTRedir *redir);
+ASTShtoken *new_ast_shtoken(enum ShtokenKind kind, void *new_shtoken);
+void delete_ast_shtoken(ASTShtoken *shtoken);
 
 #endif
